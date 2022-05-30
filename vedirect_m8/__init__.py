35184c241a2fabb43049c8dc6f885d4460f23824
Eli <eli.serra173@gmail.com>
2022-05-30 05:47:17 +0200

    update version

--- vedirect_m8/__init__.py
+++ vedirect_m8/__init__.py
@@ -5,7 +5,7 @@ __copyright__ = "Copyright 2015, Janne Kario"
 __deprecated__ = False
 __license__ = "MIT"
 __status__ = "Production"
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 
 class AppFilter(logging.Filter):
