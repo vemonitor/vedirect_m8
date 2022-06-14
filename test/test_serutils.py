0056d7e3faa5c4ae69147662419e441d354768a2
mano8 <eli.serra173@gmail.com>
2022-06-14 17:35:30 +0200

    improve SerialUtils unittest class.

--- test/test_serutils.py
+++ test/test_serutils.py
@@ -1,48 +1,53 @@
-import pytest
-from vedirect_m8.serutils import SerialUtils
+# -*- coding: utf-8 -*-
+"""
+SerialUtils unittest class.
+
+Use pytest package.
+"""
+from vedirect_m8.serutils import SerialUtils as Ut
+
+__author__ = "Eli Serra"
+__copyright__ = "Copyright 2020, Eli Serra"
+__deprecated__ = False
+__license__ = "MIT"
+__status__ = "Production"
+__version__ = "1.0.0"
 
 
 class TestSerialUtils:
 
-    def setup_method(self):
-        """ setup any state tied to the execution of the given function.
-        Invoked for every test function in the module.
-        """
-        self.obj = SerialUtils()
-
-    def teardown_method(self):
-        """ teardown any state that was previously setup with a setup_function
-        call.
-        """
-        pass
-
     def test_is_key_pattern(self):
         """"""
-        assert not self.obj.is_key_pattern('_hello')
-        assert not self.obj.is_key_pattern('hel lo')
-        assert self.obj.is_key_pattern("hj_58Hyui")
+        datas = [
+            '_hello', 'hel lo', "#hj_58Hyui#",  # false
+            "hj_58Hyui"  # true
+        ]
+        tests = [x for x in datas if Ut.is_key_pattern(x)]
+        assert len(tests) == 1
 
     def test_is_serial_key_pattern(self):
         """"""
-        assert not self.obj.is_serial_key_pattern('_hello')
-        assert not self.obj.is_serial_key_pattern('hel lo')
-        assert self.obj.is_serial_key_pattern("hj_58Hyui")
-        assert self.obj.is_serial_key_pattern("#hj_58Hyui#")
+        datas = [
+            '_hello', 'hel lo',  # false
+            "hj_58Hyui", "#hj_58Hyui#"  # true
+        ]
+        tests = [x for x in datas if Ut.is_serial_key_pattern(x)]
+        assert len(tests) == 2
 
     def test_is_unix_serial_port_pattern(self):
         """"""
-        assert not self.obj.is_unix_serial_port_pattern('/etc/ttyUSB1')
-        assert not self.obj.is_unix_serial_port_pattern("/dev/tty")
-        assert not self.obj.is_unix_serial_port_pattern("/dev/tty1")
-        assert self.obj.is_unix_serial_port_pattern("/tmp/vmodem1")
-        assert self.obj.is_unix_serial_port_pattern("/dev/ttyUSB3")
+        datas = [
+            '/etc/ttyUSB1', "/dev/tty", "/dev/tty1",  # false
+            "/dev/ttyACM1", "/dev/ttyUSB3"  # true
+        ]
+        tests = [x for x in datas if Ut.is_unix_serial_port_pattern(x)]
+        assert len(tests) == 2
 
     def test_is_win_serial_port_pattern(self):
         """"""
-        assert not self.obj.is_win_serial_port_pattern('/etc/ttyUSB1')
-        assert not self.obj.is_win_serial_port_pattern('/dev/COM3')
-        assert not self.obj.is_win_serial_port_pattern('/COM3')
-        assert not self.obj.is_win_serial_port_pattern('COM')
-        assert self.obj.is_win_serial_port_pattern("COM255")
-        assert self.obj.is_win_serial_port_pattern("COM3")
-        assert self.obj.is_win_serial_port_pattern("COM3")
+        datas = [
+            '/etc/ttyUSB1', "/dev/COM3", "/COM3", "COM"  # false
+            "COM255", "COM3", "COM0"  # true
+        ]
+        tests = [x for x in datas if Ut.is_win_serial_port_pattern(x)]
+        assert len(tests) == 2
