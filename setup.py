e9866bbbcc43b896261d2c89a35af35b8f6cd8df
mano8 <eli.serra173@gmail.com>
2022-09-27 19:55:14 +0200

    Update ve-utils requirements package from 2.5.0 to 2.5.1.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name='vedirect_m8',
       include_package_data=True,
       install_requires=[
           'pyserial>=3.5',
-          've_utils>=2.5.0',
+          've_utils>=2.5.1',
       ],
       extras_require={
               "TEST": [
