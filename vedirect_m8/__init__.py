f613227642856816f4498147d9cb40ea29846067
mano8 <eli.serra173@gmail.com>
2023-01-04 19:26:31 +0100

    Update version to 1.2.9.2

--- vedirect_m8/__init__.py
+++ vedirect_m8/__init__.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright 2015, Janne Kario"
 __deprecated__ = False
 __license__ = "MIT"
 __status__ = "Production"
-__version__ = "1.2.9.1"
+__version__ = "1.2.9.2"
 
 
 class AppFilter(logging.Filter):
