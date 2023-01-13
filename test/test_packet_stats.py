62f40416cb42fd6438d33075c2820f1ef7cf0576
mano8 <eli.serra173@gmail.com>
2023-01-13 04:38:10 +0100

    add PacketStats module and attached unit tests

---
+++ test/test_packet_stats.py
@@ -0,0 +1,115 @@
+"""PacketStats unittest class."""
+import time
+import pytest
+from vedirect_m8.packet_stats import PacketStats
+from ve_utils.utype import UType as Ut
+
+
+class TestPacketStats:
+
+    def setup_method(self):
+        """
+        Setup any state tied to the execution of the given function.
+
+        Invoked for every test function in the module.
+        """
+        self.obj = PacketStats()
+
+    @staticmethod
+    def get_dummy_packets_1():
+        """Test is_app_block method."""
+        return [
+            {"c": 2, "d": 3},
+            {"e": 4, "f": 5, "g": 6, "h": 7},
+            {"i": 8, "j": 9, "k": 10}
+        ]
+
+    @staticmethod
+    def get_dummy_packets_2():
+        """Test is_app_block method."""
+        return [
+            {"a": 0, "b": 1, "c": 2, "d": 3},
+            {"e": 4, "f": 5, "g": 6, "h": 7},
+            {"i": 8, "j": 9, "k": 10}
+        ]
+
+    def test_nb_packets(self):
+        """Test is_app_block method."""
+        packets = TestPacketStats.get_dummy_packets_1()
+        assert self.obj.add_stats(packets[0])
+        assert Ut.is_int(self.obj.get_nb_packets())
+        assert Ut.is_int(self.obj.set_nb_packets(32))
+        assert Ut.is_int(self.obj.init_nb_packets())
+
+    def test_add_stats(self):
+        """Test is_app_block method."""
+        packets = TestPacketStats.get_dummy_packets_1()
+        assert not self.obj.add_stats(packets)
+        assert not self.obj.has_stats()
+        assert self.obj.add_stats(packets[0])
+        assert self.obj.has_stats()
+        self.obj.reset_stats()
+        assert not self.obj.has_stats()
+
+    def test_is_packet_in_stats(self):
+        """Test is_app_block method."""
+        packets = TestPacketStats.get_dummy_packets_1()
+        assert not self.obj.has_stats()
+        assert self.obj.add_stats(packets[0])
+        assert self.obj.add_stats(packets[1])
+        assert self.obj.is_packet_in_stats(packets[0])
+        assert not self.obj.is_packet_in_stats(packets[2])
+        self.obj.reset_stats()
+        assert not self.obj.has_stats()
+
+    def test_set_packet_stats(self):
+        """Test set_packet_stats method."""
+        packets_1 = TestPacketStats.get_dummy_packets_1()
+        packets_2 = TestPacketStats.get_dummy_packets_2()
+        nb_packets_1 = len(packets_1)
+        nb_packets_2 = len(packets_2)
+        z = 0
+        for i in range(nb_packets_1):
+            assert self.obj.set_loop_packet_stats(i, packets_1[i])
+
+        for y in range(3):
+            for i in range(nb_packets_1):
+                assert self.obj.set_packet_stats(z, packets_1[i])
+                z = z + 1
+
+        for y in range(6):
+            for i in range(nb_packets_2):
+                result = self.obj.set_packet_stats(z, packets_2[i])
+                assert result is None\
+                    or Ut.is_dict(result, not_null=True)
+                z = z + 1
+
+    def test_set_loop_packet_stats(self):
+        """Test set_loop_packet_stats method."""
+        packets_1 = TestPacketStats.get_dummy_packets_1()
+        packets_2 = TestPacketStats.get_dummy_packets_2()
+        nb_packets_1 = len(packets_1)
+        nb_packets_2 = len(packets_2)
+        for i in range(nb_packets_1):
+            assert self.obj.set_loop_packet_stats(i, packets_1[i])
+
+        for y in range(9):
+            for i in range(nb_packets_2):
+                assert self.obj.set_loop_packet_stats(i, packets_2[i])
+        stats = self.obj.get_packet_stats_by_index(0)
+        assert stats.get('nb_resets') == 1
+
+    def test_count_linear_index(self):
+        """Test set_packet_stats method."""
+        packets = TestPacketStats.get_dummy_packets_1()
+
+        assert self.obj.add_stats(packets[0])
+        stats = self.obj.get_packet_stats_by_index(0)
+        assert self.obj.count_linear_index(
+            is_linear=True,
+            stats_packet=stats
+        ) == (1, 0)
+        assert self.obj.count_linear_index(
+            is_linear=False,
+            stats_packet=stats
+        ) == (0, 1)
---
+++ vedirect_m8/packet_stats.py
@@ -0,0 +1,205 @@
+"""
+Used to get, set and analyse serial packets stats.
+"""
+import logging
+from ve_utils.utype import UType as Ut
+
+__author__ = "Eli Serra"
+__copyright__ = "Copyright 2020, Eli Serra"
+__deprecated__ = False
+__license__ = "MIT"
+__status__ = "Production"
+__version__ = "1.0.0"
+
+logging.basicConfig()
+logger = logging.getLogger("vedirect")
+
+
+class PacketStats:
+    """
+    Used to get, set and analyse serial packets stats.
+    """
+    def __init__(self):
+        self._nb_packets = 10
+        self._max_packets_ever = 0
+        self._is_ordered_flow = False
+        self._stats = list()
+        self._count_bad_packets = 0
+
+    MAX_STAT_PACKETS = 10
+
+    def get_nb_packets(self) -> int:
+        """Get number of packets available on serial reader."""
+        return self._nb_packets
+
+    def _set_nb_packets(self, value: int) -> int:
+        """Set """
+        value = Ut.get_int(value, 0)
+        if self.MAX_STAT_PACKETS > value > 0:
+            self._nb_packets = value
+        return self._nb_packets
+
+    def init_nb_packets(self) -> int:
+        """"""
+        if self.has_stats():
+            self._set_nb_packets(len(self._stats))
+        return self._nb_packets
+
+    def has_stats(self) -> bool:
+        """"""
+        return Ut.is_list(self._stats, not_null=True)
+
+    def can_add_stats(self) -> bool:
+        """Test if _stats is not full."""
+        return Ut.is_list(self._stats)\
+            and len(self._stats) + 1 < self.MAX_STAT_PACKETS
+
+    def reset_stats(self) -> None:
+        """Reset packet _stats class property."""
+        self._stats = list()
+
+    def add_stats(self, packet: dict) -> bool:
+        """
+        Add packet _stats class property.
+        """
+        result = False
+        packet_stats = PacketStats.get_stats_from_packet(packet)
+        if Ut.is_dict(packet_stats, not_null=True)\
+                and self.can_add_stats():
+            self._stats.append(packet_stats)
+            result = True
+        return result
+
+    def get_packet_index(self, packet: dict) -> int:
+        """Get packet stat index in list."""
+        result = -1
+        packet_stats = PacketStats.get_stats_from_packet(packet)
+        if Ut.is_dict(packet_stats, not_null=True):
+            for i, last_stat in enumerate(self._stats):
+                if PacketStats.is_equal_packet_stats(stat=packet_stats, last_stat=last_stat):
+                    result = i
+        return result
+
+    def get_stats_packet_obj(self, packet: dict) -> dict:
+        """Get packet stat item dictionary."""
+        result = None
+        stats_index = self.get_packet_index(packet)
+        if stats_index >= 0:
+            result = self._stats[stats_index]
+        return result
+
+    def is_packet_in_stats(self, packet: dict) -> bool:
+        """Add packet stat"""
+        stats_index = self.get_packet_index(packet)
+        return stats_index >= 0
+
+    def get_packet_stats_by_index(self, index: int) -> dict:
+        """Add packet stat"""
+        result = None
+        index = Ut.get_int(index, -1)
+        if self.has_stats() and 0 <= index < len(self._stats):
+            result = self._stats[index]
+        return result
+
+    def set_packet_stats(self, index: int, packet: dict) -> dict:
+        """Set packet flow stats."""
+        result = None
+        stats_packet = self.get_packet_stats_by_index(index)
+        stats_equal = self.get_stats_packet_obj(packet)
+        if not Ut.is_dict(stats_packet, not_null=True)\
+                and Ut.is_dict(stats_equal, not_null=True):
+            is_linear = True
+            nb_linear, nb_resets = PacketStats.count_linear_index(
+                is_linear=is_linear,
+                stats_packet=stats_equal
+            )
+            step = index - stats_equal.get('last_index')
+            index_stats = {
+                "last_index": index,
+                "step": step,
+                "is_linear": is_linear,
+                "nb_linear": nb_linear,
+                "nb_resets": nb_resets
+            }
+            stats_equal.update(index_stats)
+            result = stats_equal
+        elif Ut.is_dict(stats_packet, not_null=True):
+            new_stats = PacketStats.get_stats_from_packet(packet)
+            is_packet_linear = PacketStats.is_equal_packet_stats(
+                stat=new_stats,
+                last_stat=stats_packet
+            )
+            is_index_linear = PacketStats.is_index_linear(
+                index=index,
+                last_index=stats_packet.get('last_index')
+            )
+            is_linear = is_packet_linear and is_index_linear
+            nb_linear, nb_resets = PacketStats.count_linear_index(
+                is_linear=is_linear,
+                stats_packet=stats_packet
+            )
+            index_stats = {
+                "last_index": index,
+                "is_linear": is_linear,
+                "nb_linear": nb_linear,
+                "nb_resets": nb_resets
+            }
+            if not is_linear:
+                stats_packet.update(new_stats)
+            stats_packet.update(index_stats)
+            result = stats_packet
+
+        return result
+
+    def set_loop_packet_stats(self, index: int, packet: dict) -> bool:
+        """"""
+        result = False
+        stats_packet = self.set_packet_stats(
+            index=index,
+            packet=packet
+        )
+        if not Ut.is_dict(stats_packet, not_null=True)\
+                and self.add_stats(packet):
+            stats_packet = self.set_packet_stats(
+                index=index,
+                packet=packet
+            )
+
+        if Ut.is_dict(stats_packet, not_null=True):
+            result = True
+        return result
+
+    @staticmethod
+    def get_stats_from_packet(packet: dict) -> dict:
+        """
+        Get packet stats from serial packet.
+        Retrieve number and list of keys in packet.
+        """
+        result = None
+        if Ut.is_dict(packet, not_null=True):
+            result = {'nb': len(packet), 'keys': list(packet.keys())}
+        return result
+
+    @staticmethod
+    def is_equal_packet_stats(stat: dict, last_stat: dict or None) -> bool:
+        """Test if actual packet stats is equal to registered packet stats."""
+        return Ut.is_dict(stat, not_null=True)\
+            and stat.get('nb') > 0\
+            and stat.get('nb') == last_stat.get('nb')\
+            and stat.get('keys') == last_stat.get('keys')
+
+    @staticmethod
+    def is_index_linear(index: int, last_index: int or None) -> bool:
+        """Test if actual index is equal to last index."""
+        last_index = Ut.get_int(last_index, default=index)
+        return last_index == index
+
+    @staticmethod
+    def count_linear_index(is_linear: bool, stats_packet: dict) -> tuple:
+        """Add packet stat"""
+        nb_linear = Ut.get_int(stats_packet.get('nb_linear'), 0)
+        nb_resets = Ut.get_int(stats_packet.get('nb_resets'), 0)
+        if is_linear is True:
+            return nb_linear + 1, nb_resets
+        else:
+            return 0, nb_resets + 1
