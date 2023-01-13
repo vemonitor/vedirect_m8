57902c5e7d7f53bde1d9526e41e3ba6c9d63313f
mano8 <eli.serra173@gmail.com>
2023-01-13 03:13:04 +0100

    reorder exceptions available in package.

--- vedirect_m8/exceptions.py
+++ vedirect_m8/exceptions.py
@@ -13,33 +13,76 @@ __status__ = "Production"
 __version__ = "1.0.0"
 
 
-class SettingInvalidException(Exception):
-    """
-    Some data must match the expected value/type
-
-    .. seealso:: Settings
-    """
-
-
-class InputReadException(Exception):
-    """
-    Input read fails
-
-    .. seealso:: Settings
-    """
-
-
-class TimeoutException(Exception):
-    """
-    Serial Timeout Exception
-
-    .. seealso:: Settings
-    """
-
-
 class VedirectException(Exception):
     """
-    Serial Vedirect decoder Exception
-
-    .. seealso:: Settings
+    Serial Vedirect core exception.
     """
+
+
+class SettingInvalidException(VedirectException):
+    """
+    Some data must match the expected value/type
+    """
+
+
+class VeReadException(VedirectException):
+    """
+    VeDirect Read Exception.
+    Extends: VeCoreException
+    """
+
+
+class InputReadException(VeReadException):
+    """
+    Input read fails.
+    Extends: VeCoreException, VeReadException
+    """
+
+
+class PacketReadException(VeReadException):
+    """
+    Packet read fails.
+    Extends: VeCoreException, VeReadException
+    """
+
+
+class ReadTimeoutException(VeReadException):
+    """
+    Serial Read Timeout Exception.
+    Extends: VeCoreException, VeReadException
+    """
+
+
+class SerialConnectionException(VedirectException):
+    """
+    Serial Vedirect Exception.
+    Extends: VeCoreException
+    """
+
+
+class SerialConfException(SerialConnectionException):
+    """
+    Serial Configuration Exception.
+    """
+
+
+class SerialVeTimeoutException(SerialConnectionException):
+    """
+    Serial Timeout Exception.
+    Extends: VeCoreException, SerialConnexionException
+    """
+
+
+class SerialVeException(SerialConnectionException):
+    """
+    Serial Vedirect device can not be found or can not be configured.
+    Extends: VeCoreException, SerialConnexionException
+    """
+
+
+class OpenSerialVeException(SerialConnectionException):
+    """
+    Serial Vedirect device configured but connection not opened.
+    Extends: VeCoreException, SerialConnexionException
+    """
+
