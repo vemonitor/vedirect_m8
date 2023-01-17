cc8dc9f0149dd41ada6511e02d1a196f6670e182
mano8 <eli.serra173@gmail.com>
2023-01-18 00:34:38 +0100

    correct bug.

--- test/test_ve_controller.py
+++ test/test_ve_controller.py
@@ -259,7 +259,7 @@ class TestVedirectController:
 
         self.ve_sim.run_vedirect_sim_callback(
             callback=main_test,
-            nb_packets=20,
+            nb_packets=40,
             sleep=0.5
         )
 
