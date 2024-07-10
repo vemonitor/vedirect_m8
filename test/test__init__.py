c1692b25a2bc41ef0a7e2baf1ac16d7e3b5f1d77
zepi <eli.serra173@gmail.com>
2024-07-11 00:52:12 +0200

    add configure_logging unit tests

---
+++ test/test__init__.py
@@ -0,0 +1,18 @@
+import pytest
+import logging
+from vedirect_m8 import configure_logging
+
+logging.basicConfig()
+logger = logging.getLogger("vedirect")
+
+class TestInit:
+
+    def test_logger_debug(self):
+        """"""
+        configure_logging(True)
+        assert logger.level == logging.DEBUG
+    
+    def test_logger_default(self):
+        """"""
+        configure_logging()
+        assert logger.level == logging.INFO
