75f3ecab9a10e5080d135e6168a7a6c1dc7657a5
zepi <eli.serra173@gmail.com>
2024-08-07 02:33:32 +0200

    Add vepackets_app module

---
+++ test/test_vepackets_app.py
@@ -0,0 +1,277 @@
+"""VePackets unittest class."""
+import time
+import pytest
+from ve_utils.utype import UType as Ut
+from vedirect_m8.serconnect import SerialConnection
+from vedirect_m8.vepackets_app import VePacketsApp
+from vedirect_m8.exceptions import SerialConnectionException
+
+
+@pytest.fixture(name="helper_manager", scope="class")
+def helper_manager_fixture():
+    """Json Schema test manager fixture"""
+    class HelperManager:
+        """Json Helper test manager fixture Class"""
+        def __init__(self):
+            self.obj = None
+
+        def init_vedirect_app(self):
+            """Init Object"""
+            conf = {
+                'serial_port': SerialConnection.get_virtual_home_serial_port(
+                    "vmodem1"
+                ),
+                'baud': 19200,
+                'timeout': 0
+            }
+            serial_test = {
+                'PIDTest': {
+                    "typeTest": "value",
+                    "key": "PID",
+                    "value": "0x203"
+                }
+            }
+
+            self.obj = VePacketsApp(
+                serial_conf=conf,
+                serial_test=serial_test,
+                source_name="PyTest",
+                auto_start=True,
+                wait_connection=True,
+                wait_timeout=5
+            )
+
+        def read_serial_data(self):
+            """Read Serial Data"""
+            assert self.obj.try_serial_connection(
+                "PyTest"
+            ) is True
+            assert self.obj.has_data_cache() is False
+            result = self.obj.read_data(
+                caller_name="PyTest",
+                timeout=2
+            )
+            assert Ut.is_dict(result, eq=26)
+            assert self.obj.has_data_cache() is True
+
+    return HelperManager()
+
+
+class TestVePacketsApp:
+    """VePacketsApp unittest class."""
+
+    def test_settings(self, helper_manager):
+        """Test configuration settings from VedirectController constructor."""
+        helper_manager.init_vedirect_app()
+
+        assert helper_manager.obj.is_serial_ready()
+        assert helper_manager.obj.is_ready()
+        assert helper_manager.obj.has_serial_com()
+        assert helper_manager.obj.connect_to_serial()
+        assert helper_manager.obj.has_serial_test()
+        assert helper_manager.obj.get_wait_timeout()
+        helper_manager.obj._com = None
+        with pytest.raises(SerialConnectionException):
+            helper_manager.obj.connect_to_serial()
+
+    def test_has_data_cache(self, helper_manager):
+        """Test has_items method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+    def test_get_time_cache(self, helper_manager):
+        """Test get_time_cache method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        time_cache = helper_manager.obj.get_time_cache()
+        assert Ut.is_float(time_cache, not_null=True)
+
+    def test_get_data_cache(self, helper_manager):
+        """Test get_data_cache method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        result = helper_manager.obj.get_data_cache()
+        assert Ut.is_dict(result, eq=26)
+
+    def test_set_min_interval(self, helper_manager):
+        """Test set_min_interval method """
+        helper_manager.init_vedirect_app()
+
+        assert helper_manager.obj.set_min_interval(
+            value=1
+        ) == 1
+
+        assert helper_manager.obj.set_min_interval(
+            value=0
+        ) == 1
+
+        assert helper_manager.obj.set_min_interval(
+            value=-1
+        ) == 1
+
+        assert helper_manager.obj.set_min_interval(
+            value=99
+        ) == 99
+
+    def test_reset_data_cache(self, helper_manager):
+        """Test reset_data_cache method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        result = helper_manager.obj.get_data_cache()
+        assert Ut.is_dict(result, eq=26)
+        helper_manager.obj.reset_data_cache()
+
+        result = helper_manager.obj.get_data_cache()
+        assert result is None
+
+    def test_set_data_cache(self, helper_manager):
+        """Test set_data_cache method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        assert helper_manager.obj.has_data_cache() is True
+        time_cache_start = helper_manager.obj.get_time_cache()
+        result = helper_manager.obj.set_data_cache(
+            data={
+                'V': 15,
+                'I': 14
+            }
+        )
+        assert result is True
+        time_cache_end = helper_manager.obj.get_time_cache()
+        assert time_cache_start != time_cache_end
+
+        result = helper_manager.obj.get_data_cache()
+        assert Ut.is_dict(result, eq=2)
+
+    def test_update_data_cache(self, helper_manager):
+        """Test update_data_cache method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        time_cache_start = helper_manager.obj.get_time_cache()
+        assert helper_manager.obj.has_data_cache() is True
+        result = helper_manager.obj.update_data_cache(
+            data={
+                'V': 15,
+                'I': 14
+            }
+        )
+        assert result is True
+        time_cache_end = helper_manager.obj.get_time_cache()
+        assert time_cache_start == time_cache_end
+
+        result = helper_manager.obj.get_data_cache()
+        assert Ut.is_dict(result, eq=26)
+
+    def test_search_available_serial_port(self, helper_manager):
+        """Test search_available_serial_port method """
+        helper_manager.init_vedirect_app()
+
+        assert helper_manager.obj.search_available_serial_port(
+            caller_name="PyTest"
+        ) is True
+
+        # Change to bad serial port and close serial connexion
+        helper_manager.obj._com._serial_port = "/dev/vmodem8"
+        helper_manager.obj._com.ser.close()
+        # search and connect to valid serial port
+        assert helper_manager.obj.search_available_serial_port(
+            caller_name="PyTest"
+        ) is True
+
+    def test_try_serial_connection(self, helper_manager):
+        """Test try_serial_connection method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        # Test try_serial_connection unlocked serial
+        result = helper_manager.obj.try_serial_connection(
+            caller_name="PyTest"
+        )
+        assert result is True
+
+        # Test try_serial_connection with bad serial port
+        # Search and connect to new valid serial port
+        helper_manager.obj._com._serial_port = "/dev/vmodem8"
+        helper_manager.obj._com.ser.close()
+        result = helper_manager.obj.try_serial_connection(
+            caller_name="PyTest"
+        )
+        assert result is True
+
+    def test_read_serial_data(self, helper_manager):
+        """Test read_serial_data method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+        time.sleep(1)
+        result = helper_manager.obj.read_serial_data(
+            caller_name="PyTest",
+            timeout=3
+        )
+        assert Ut.is_dict(result, not_null=True, max_items=19)
+        # Try SerialVeTimeoutException
+        result = helper_manager.obj.read_serial_data(
+            caller_name="PyTest",
+            timeout=0.0001
+        )
+        assert result is None
+        # Try search new valid serial port
+        helper_manager.obj._com._serial_port = "/dev/vmodem8"
+        helper_manager.obj._com.ser.close()
+        result = helper_manager.obj.read_serial_data(
+            caller_name="PyTest",
+            timeout=0.0001
+        )
+        assert result is None
+
+    def test_get_all_packets(self, helper_manager):
+        """Test get_all_packets method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        helper_manager.obj.reset_data_cache()
+        assert helper_manager.obj.has_data_cache() is False
+        result = helper_manager.obj.get_all_packets(
+            caller_name="PyTest",
+            timeout=3
+        )
+        assert result is True
+        assert helper_manager.obj.has_data_cache() is True
+
+    def test_is_time_to_read_serial(self, helper_manager):
+        """Test is_time_to_read_serial method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        is_time_cache = helper_manager.obj.is_time_to_read_serial(
+            now=time.time()
+        )
+        assert is_time_cache is False
+
+    def test_read_data(self, helper_manager):
+        """Test read_data method """
+        helper_manager.init_vedirect_app()
+        helper_manager.read_serial_data()
+
+        # get data from serial
+        time.sleep(1)
+        helper_manager.obj.reset_data_cache()
+        assert helper_manager.obj.has_data_cache() is False
+        result = helper_manager.obj.read_data(
+            caller_name="PyTest",
+            timeout=3
+        )
+        assert Ut.is_dict(result, not_null=True, eq=26)
+        # get data from cache
+        # because two read_data calls executed
+        # in less than 1 second
+        result2 = helper_manager.obj.read_data(
+            caller_name="PyTest",
+            timeout=3
+        )
+        assert Ut.is_dict(result2, not_null=True, eq=26)
+        assert result == result2
---
+++ vedirect_m8/vepackets_app.py
@@ -0,0 +1,427 @@
+"""vepackets module"""
+import logging
+import time
+from typing import Optional, Union
+from ve_utils.utype import UType as Ut
+from vedirect_m8.exceptions import InputReadException
+from vedirect_m8.exceptions import SerialVeTimeoutException
+from vedirect_m8.exceptions import VedirectException
+from vedirect_m8.ve_controller import VedirectController
+from vedirect_m8.packet_stats import PacketStats
+
+__author__ = "Eli Serra"
+__copyright__ = "Copyright 2020, Eli Serra"
+__license__ = "MIT"
+
+logging.basicConfig()
+logger = logging.getLogger("vedirect")
+
+
+class VePacketsApp(VedirectController):
+    """
+        Extends VedirectController and minitor reading packets from Vedirect
+        Some vedirect devices send many packets of data
+        composed by between 0 and 18 blocks.
+        Here we get all packets frome same timestamp and set stats
+        of reading stability on VeDirect Serial port.
+    """
+    def __init__(self,
+                 serial_conf: dict,
+                 serial_test: dict,
+                 source_name: str = 'VePackets',
+                 auto_start: bool = True,
+                 wait_connection: bool = True,
+                 wait_timeout: Union[int, float] = 3600,
+                 max_packet_blocks: Optional[int] = 18,
+                 nb_packets: int = 10,
+                 accepted_keys: Optional[list] = None,
+                 min_interval: int = 1,
+                 max_read_error: int = 30
+                 ):
+        """
+        Constructor of VedirectController class.
+
+        :Example:
+            - > sc = VedirectController(serial_port = "/dev/ttyUSB1")
+            - > sc.connect()
+            - > True # if connection opened on serial port "/dev/tyyUSB1"
+        :param self: Refer to the object instance itself,
+        :param serial_conf: dict: The serial connection configuration,
+        :param serial_test:
+            The serial_test to execute to retrieve the serial port,
+        :param source_name:
+            This is used in logger to identify the source of call,
+        :param auto_start: bool:
+            Define if serial connection must be established automatically,
+        :param wait_connection: bool:
+            Used to wait for connection on new serial port at start,
+        :param wait_timeout:
+            Timeout value to search valid serial port,
+            in case of connection fails
+        :param max_packet_blocks:
+            Max blocks per packet value
+        :param packet_keys:
+            List of accepted packet keys
+        :return: Nothing
+        """
+        VedirectController.__init__(
+            self,
+            serial_conf=serial_conf,
+            serial_test=serial_test,
+            source_name=source_name,
+            auto_start=auto_start,
+            wait_connection=wait_connection,
+            wait_timeout=wait_timeout,
+            max_packet_blocks=max_packet_blocks
+        )
+        self._data_cache = None
+        self._min_interval = 1
+        self.packets_stats = PacketStats(
+            nb_packets=nb_packets,
+            accepted_keys=accepted_keys,
+            max_read_error=max_read_error
+        )
+        self.set_min_interval(min_interval)
+
+    def has_data_cache(self) -> bool:
+        """Test if instance has data_cache"""
+        return Ut.is_tuple(self._data_cache)\
+            and Ut.is_numeric(
+                self._data_cache[0],  # type: ignore
+                not_null=True)\
+            and Ut.is_dict(
+                self._data_cache[1],  # type: ignore
+                not_null=True)
+
+    def get_time_cache(self) -> Optional[float]:
+        """Get data_cache time value"""
+        result = None
+        if self.has_data_cache():
+            result = self._data_cache[0]  # type: ignore
+        return result
+
+    def get_data_cache(self) -> Optional[dict]:
+        """Get data_cache value"""
+        result = None
+        if self.has_data_cache():
+            result = self._data_cache[1]  # type: ignore
+        return result
+
+    def set_min_interval(self, value: int):
+        """Set min_interval"""
+        min_interval = Ut.get_int(value, 1)
+        if min_interval >= 1:
+            self._min_interval = min_interval
+        return self._min_interval
+
+    def reset_data_cache(self):
+        """Reset data_cache"""
+        self._data_cache = None
+
+    def set_data_cache(self,
+                       data: Optional[dict]
+                       ) -> bool:
+        """Init cache tuple values."""
+        result = False
+        self._data_cache = None
+        if Ut.is_dict(data, not_null=True):
+            self._data_cache = (time.time(), data)
+            result = True
+        return result
+
+    def update_data_cache(self,
+                          data: Optional[dict]
+                          ) -> bool:
+        """Add data to cache."""
+        result = False
+        if self.has_data_cache():
+            if Ut.is_dict(data, not_null=True):
+                self._data_cache[1].update(data)  # type: ignore
+                result = True
+        else:
+            result = self.set_data_cache(data)
+        return result
+
+    def search_available_serial_port(self,
+                                     caller_name: str,
+                                     raise_exception: bool = True
+                                     ) -> bool:
+        """
+        Search and connect to new valid Serial Port if it's available
+        """
+        result = False
+        if self.search_serial_port():
+            logger.warning(
+                "%s: Connected to serial port %s.",
+                caller_name,
+                self.get_serial_port()
+            )
+            result = True
+        elif raise_exception is True:
+            raise VedirectException(
+                "Fatal Error: "
+                "Unable to connect to any serial port. "
+                f"caller: {caller_name}"
+            )
+        return result
+
+    def try_serial_connection(self, caller_name: str) -> bool:
+        """
+        Try to connect to serial port, and run a serial test on it.
+        """
+        result = False
+        try:
+            if self.connect_to_serial()\
+                    and self.get_serial_port() is not None\
+                    and self.test_serial_port(
+                        port=self.get_serial_port()  # type: ignore
+                    ):
+                logger.info(
+                    "Serial port %s is ready. ",
+                    self.get_serial_port()
+                )
+                result = True
+        except VedirectException as ex:
+            logger.debug(
+                "[VedirectApp::try_serial_connection] "
+                "Unable to connect to serial port %s.\n"
+                "ex: %s",
+                self.get_serial_port(),
+                ex
+            )
+            logger.info("Searching for available serial port.")
+            if self.search_available_serial_port(
+                    caller_name=caller_name,
+                    raise_exception=False
+                    ):
+                result = True
+        return result
+
+    def try_to_reconnect(self,
+                         caller_name: str,
+                         from_exception: Optional[Exception] = None
+                         ) -> bool:
+        """
+        Try to connect to serial port, and run a serial test on it.
+        """
+        if not self.try_serial_connection(caller_name):
+            logger.error(
+                "[VedirectApp::try_to_reconnect] "
+                "Unable to get valid Serial port."
+            )
+            msg = "[Vedirect::try_to_reconnect] "\
+                "Fatal error on reading serial port "\
+                f"{self.get_serial_port()}"
+            if isinstance(from_exception, Exception):
+                raise VedirectException(msg) from from_exception
+
+            raise VedirectException(msg)
+        return True
+
+    def read_serial_data(self,
+                         caller_name: str,
+                         timeout: int = 2
+                         ) -> Optional[dict]:
+        """
+        Get single packet from VeDirect controller.
+        """
+        result = None
+        if self.is_ready():
+            try:
+                timeout = Ut.get_int(timeout, default=2)
+                result = self.read_data_single(timeout=timeout)
+            except (
+                    VedirectException,
+                    SerialVeTimeoutException
+            ) as ex:
+                # Here we try the actual serial connexion
+                # And if fails try to search and connect to new serial port
+                self.try_to_reconnect(
+                    caller_name=caller_name,
+                    from_exception=ex
+                )
+        else:
+            self.try_to_reconnect(
+                caller_name=caller_name
+            )
+        return result
+
+    def get_all_packets(self,
+                        caller_name: str,
+                        timeout: int = 2
+                        ) -> bool:
+        """
+        Get All packets from from VeDirect controller.
+        Most VeDirect Devices has many packets to read.
+        Every packet contain 0-18 blocks.
+        """
+        result = False
+        errors, blocks = 0, []
+        nb_packets = self.packets_stats.get_nb_packets()
+        max_read_error = nb_packets
+        self.reset_data_cache()
+
+        for i in range(nb_packets):
+            read_errors = self.packets_stats.get_serial_read_errors()
+            try:
+                tmp = self.read_serial_data(caller_name, timeout)
+            except InputReadException as ex:
+                if read_errors >= max_read_error:
+                    raise VedirectException(
+                        "[Vedirect::input_read] "
+                        "Serial input read error"
+                    ) from ex
+                logger.debug(
+                    "[VedirectApp::read_data] "
+                    "Serial read Error n° %s ex : %s",
+                    read_errors,
+                    ex
+                )
+                self.packets_stats.add_serial_read_errors()
+
+            if self.update_data_cache(tmp):
+                self.packets_stats.set_loop_packet_stats(
+                    index=i,
+                    packet=tmp  # type: ignore
+                )
+                blocks.append(len(tmp))  # type: ignore
+                time.sleep(0.005)
+            else:
+                errors = errors + 1
+
+        if self.has_data_cache():
+            self.packets_stats.init_nb_packets()
+            result = True
+            logger.debug(
+                "[VedirectApp::get_serial_packet] "
+                "Read %s blocks from serial. "
+                "len: %s - blocks: %s - "
+                "errors: %s - time ref: %s \n"
+                "result: %s",
+                i+1,
+                len(self.get_data_cache()),  # type: ignore
+                blocks,
+                errors,
+                self.get_time_cache(),
+                self.get_data_cache()
+            )
+        return result
+
+    def is_time_to_read_serial(self,
+                               now: float,
+                               min_interval: int = 1
+                               ) -> bool:
+        """Test if is time to read from serial"""
+        result = True
+        time_cache = self.get_time_cache()
+        if Ut.is_numeric(time_cache, positive=True)\
+                and Ut.is_numeric(now, positive=True)\
+                and Ut.is_numeric(min_interval, positive=True):
+            max_i = Ut.get_int(now) + min_interval
+            result = time_cache > max_i
+            logger.debug(
+                "[VedirectApp::is_time_to_read_serial] "
+                "Evaluate cache validity: %s.\n"
+                "%s <= %s",
+                result,
+                time_cache,
+                max_i
+            )
+
+        return result
+
+    def read_serial_packets(self,
+                            caller_name: str,
+                            now: int,
+                            timeout: int = 2
+                            ) -> Optional[dict]:
+        """Read serial packets from serial."""
+        self.init_data_read()
+        # reset cache
+        self.reset_data_cache()
+        if self.get_all_packets(caller_name, timeout):
+            result = self.get_data_cache()
+            if not Ut.is_dict(result, not_null=True):
+                logger.debug(
+                    "[VedirectApp::read_data] "
+                    "Error: Unable read packets from serial. "
+                    "Time packet: %s \n"
+                    "Time diff: %s \n",
+                    self.get_time_cache(),
+                    self.get_time_cache_diff(
+                        time_cache=self.get_time_cache(),
+                        now=now
+                    )  # type: ignore
+                )
+                self.packets_stats.add_serial_read_errors()
+                result = None
+
+            logger.debug(
+                "[VedirectApp::read_data] Read data from serial. "
+                "worker: %s - time: %s  \n"
+                "Time packet: %s \n"
+                "Time diff: %s \n"
+                "Data Dict: %s \n",
+                caller_name,
+                now,
+                self.get_time_cache(),
+                self.get_time_cache_diff(
+                    time_cache=self.get_time_cache(),
+                    now=now
+                ),  # type: ignore
+                result
+            )
+        else:
+            self.packets_stats.add_serial_read_errors()
+        # Control if has reached max serial read errors
+        # raise InputReadException if reached max value.
+        self.packets_stats.has_reached_max_errors()
+        return result
+
+    def read_data(self,
+                  caller_name: str,
+                  timeout: int = 2
+                  ) -> Optional[dict]:
+        """Read data"""
+        result = None
+        now = time.time()
+        logger.debug(
+            "[VedirectApp::read_data] Read vedirect data."
+            "worker: %s - time: %s",
+            caller_name,
+            now
+        )
+        if self.is_time_to_read_serial(now):
+            result = self.read_serial_packets(
+                caller_name=caller_name,
+                now=now,
+                timeout=timeout
+            )
+
+        else:
+            result = self.get_data_cache()
+            logger.debug(
+                "[VedirectApp::read_data] Read data from cache."
+                "worker: %s - time: %s  \n"
+                "Time packet: %s \n"
+                "Time diff: %s \n",
+                caller_name,
+                now,
+                self.get_time_cache(),
+                self.get_time_cache_diff(
+                    time_cache=self.get_time_cache(),
+                    now=now
+                )  # type: ignore
+            )
+        return result
+
+    @staticmethod
+    def get_time_cache_diff(time_cache: Union[int, float],
+                            now: Union[int, float]
+                            ) -> Union[int, float]:
+        """Test if actual index is equal to last index."""
+        result = -1
+        if Ut.is_numeric(time_cache, positive=True)\
+                and Ut.is_numeric(now, positive=True):
+            result = time_cache - now
+        return result
