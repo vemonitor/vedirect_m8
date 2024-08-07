e4709072378a371493d54fd628d2085f1e403367
zepi <eli.serra173@gmail.com>
2024-08-07 14:46:16 +0200

    set version to v1.3.2.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='vedirect_m8',
-      version='1.3.2.1',
+      version='1.3.2.2',
       packages=['vedirect_m8'],
       package_dir={
             'vedirect_m8': 'vedirect_m8'
